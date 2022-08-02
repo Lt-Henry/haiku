f7d9453e8a25ef4a2f33281ddd4de74e9e188406
Lt-Henry <quiqueiii@gmail.com>
2022-08-02 10:00:40 +0200

    driver skeleton
    Change-Id: Ic62b3c3ef1d5f91d7e5ffe7d7553bd56b1cf85a5

---
+++ src/add-ons/kernel/drivers/input/hid/Driver.cpp
@@ -0,0 +1,199 @@
+/*
+ * Copyright 2005-2009 Haiku, Inc.
+ * Distributed under the terms of the MIT License.
+ *
+ * USB hid raw device driver
+ *
+ * Authors (in chronological order):
+ *		Lt_Henry
+ */
+
+#include <KernelExport.h>
+#include <Drivers.h>
+#include <USB3.h>
+#include <util/kernel_cpp.h>
+#include <util/Vector.h>
+#include <lock.h>
+
+#define TRACE(x...)	dprintf("usb_hid_raw" ": " x)
+
+#define USB_INTERFACE_CLASS_HID			3
+
+int32 api_version = B_CUR_DRIVER_API_VERSION;
+
+usb_module_info *gUSBModule;
+static int32 sParentCookie = 0;
+static mutex sDriverLock;
+
+/* usb module hooks */
+
+status_t
+usb_hid_device_added(usb_device device, void **cookie)
+{
+	TRACE("device_added\n");
+
+	const usb_device_descriptor *deviceDescriptor
+		= gUSBModule->get_device_descriptor(device);
+
+	TRACE("vendor id: 0x%04x; product id: 0x%04x\n",
+		deviceDescriptor->vendor_id, deviceDescriptor->product_id);
+
+	const usb_configuration_info *config
+		= gUSBModule->get_nth_configuration(device, USB_DEFAULT_CONFIGURATION);
+	if (config == NULL) {
+		TRACE("cannot get default configuration\n");
+		return B_ERROR;
+	}
+
+	// ensure default configuration is set
+	status_t result = gUSBModule->set_configuration(device, config);
+	if (result != B_OK) {
+		TRACE_ALWAYS("set_configuration() failed 0x%08" B_PRIx32 "\n", result);
+		return result;
+	}
+
+	// refresh config
+	config = gUSBModule->get_configuration(device);
+	if (config == NULL) {
+		TRACE_ALWAYS("cannot get current configuration\n");
+		return B_ERROR;
+	}
+
+	for (size_t i = 0; i < config->interface_count; i++) {
+		const usb_interface_info *interface = config->interface[i].active;
+		uint8 interfaceClass = interface->descr->interface_class;
+		TRACE("interface %" B_PRIuSIZE ": class: %u; subclass: %u; protocol: "
+			"%u\n",	i, interfaceClass, interface->descr->interface_subclass,
+			interface->descr->interface_protocol);
+
+		if (interfaceClass == USB_INTERFACE_CLASS_HID) {
+			mutex_lock(&sDriverLock);
+			
+			mutex_unlock(&sDriverLock);
+		}
+
+	}
+
+	return B_OK;
+}
+
+status_t
+usb_hid_device_removed(void *cookie)
+{
+	TRACE("device_removed\n");
+	return B_OK;
+}
+
+/* device hooks */
+
+static status_t
+usb_hid_open(const char *name, uint32 flags, void **_cookie)
+{
+	return B_OK;
+}
+
+static status_t
+usb_hid_read(void *_cookie, off_t position, void *buffer, size_t *numBytes)
+{
+	return B_OK;
+}
+
+static status_t
+usb_hid_write(void *_cookie, off_t position, const void *buffer,
+	size_t *numBytes)
+{
+	return B_OK;
+}
+
+static status_t
+usb_hid_control(void *_cookie, uint32 op, void *buffer, size_t length)
+{
+	return B_OK;
+}
+
+static status_t
+usb_hid_close(void *_cookie)
+{
+	return B_OK;
+}
+
+static status_t
+usb_hid_free(void *_cookie)
+{
+	return B_OK;
+}
+
+/* driver hooks */
+
+status_t
+init_hardware(void)
+{
+	TRACE("init_hardware\n");
+	return B_OK;
+}
+
+
+const char **
+publish_devices(void)
+{
+	TRACE("publish_devices\n");
+	return NULL;
+}
+
+
+device_hooks *
+find_device(const char *name)
+{
+	TRACE("find_device:%s\n",name);
+
+	static device_hooks hooks = {
+		usb_hid_open,
+		usb_hid_close,
+		usb_hid_free,
+		usb_hid_control,
+		usb_hid_read,
+		usb_hid_write,
+		NULL,				/* select */
+		NULL				/* deselect */
+	};
+
+	return &hooks;
+}
+
+
+status_t
+init_driver(void)
+{
+	TRACE("init_driver\n");
+	if (get_module(B_USB_MODULE_NAME, (module_info **)&gUSBModule) != B_OK)
+		return B_ERROR;
+
+	mutex_init(&sDriverLock, "usb hid raw driver lock");
+
+	static usb_support_descriptor genericHIDSupportDescriptor = {
+		USB_INTERFACE_CLASS_HID, 0, 0, 0, 0
+	};
+
+	gUSBModule->register_driver("usb_hid_raw", &genericHIDSupportDescriptor,
+			1, NULL);
+
+	static usb_notify_hooks notifyHooks = {
+		&usb_hid_device_added,
+		&usb_hid_device_removed
+	};
+
+	gUSBModule->install_notify("usb_hid_raw", &notifyHooks);
+
+	return B_OK;
+}
+
+
+void
+uninit_driver(void)
+{
+	TRACE("uninit_driver\n");
+
+	gUSBModule->uninstall_notify("usb_hid_raw");
+	put_module(B_USB_MODULE_NAME);
+	mutex_destroy(&sDriverLock);
+}
