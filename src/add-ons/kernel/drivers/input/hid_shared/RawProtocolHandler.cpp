465e95ff3ea840aebbdd1d25a53b516cc26a9fe3
Lt-Henry <quiqueiii@gmail.com>
2023-04-02 00:22:53 +0200

    hid raw protocol handler
    Change-Id: I8e20a5d9a4f9cbd9f05a49209d365ef33d84a48f

---
+++ src/add-ons/kernel/drivers/input/hid_shared/RawProtocolHandler.cpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2022 Enrique Medina Gremaldos <quiqueiii@gmail.com>
+ * Distributed under the terms of the MIT license.
+ */
+
+#include "Driver.h"
+#include "RawProtocolHandler.h"
+
+#include "HIDCollection.h"
+#include "HIDDevice.h"
+#include "HIDReport.h"
+#include "HIDReportItem.h"
+
+#include <new>
+#include <kernel.h>
+#include <string.h>
+#include <usb/USB_hid.h>
+
+#include <keyboard_mouse_driver.h>
+
+
+RawProtocolHandler::RawProtocolHandler(HIDReport &report) :
+	ProtocolHandler(report.Device(), "input/hid/" DEVICE_PATH_SUFFIX "/", 0),
+	fReport(report)
+{
+	TRACE("raw HID handler at %s\n",PublishPath());
+}
+
+void
+RawProtocolHandler::AddHandlers(HIDDevice &device, HIDCollection &collection,
+	ProtocolHandler *&handlerList)
+{
+	uint32 inputReportCount = 0;
+	HIDReport *inputReports[maxReportCount];
+	collection.BuildReportList(HID_REPORT_TYPE_INPUT, inputReports,
+		inputReportCount);
+
+	for (uint32 i = 0; i < inputReportCount; i++) {
+		HIDReport *inputReport = inputReports[i];
+
+		ProtocolHandler *newHandler = new(std::nothrow) RawProtocolHandler(
+			*inputReport);
+
+		if (newHandler == NULL) {
+			TRACE("failed to allocated raw protocol handler\n");
+			continue;
+		}
+
+		newHandler->SetNextHandler(handlerList);
+		handlerList = newHandler;
+	}
+}
+
+status_t
+RawProtocolHandler::Control(uint32 *cookie, uint32 op, void *buffer,
+									size_t length) {
+
+	switch (op) {
+
+		case B_GET_DEVICE_NAME:
+		{
+			const char name[] = DEVICE_NAME" HID Raw";
+			return IOGetDeviceName(name,buffer,length);
+		}
+		break;
+
+		case HID_GET_REPORT_DESCRIPTOR:
+		{
+			
+		}
+		break;
+	}
+
+	return B_ERROR;
+}
+
+status_t
+RawProtocolHandler::Read(uint32 *cookie, off_t position,void *buffer,
+								size_t *numBytes) {
+
+	uint8	tmp[64];
+	status_t status = _ReadReport(tmp,cookie);
+	sprintf(tmp,"report size:%" B_PRIu32 "\n",Device().Parser().ReportSize());
+	if (user_strlcpy((char *)buffer, tmp, strlen(tmp)) > 0)
+		return B_OK;
+
+	return B_ERROR;
+}
+
+status_t
+RawProtocolHandler::_ReadReport(void *buffer, uint32 *cookie)
+{
+	status_t result = fReport.WaitForReport(B_INFINITE_TIMEOUT);
+
+	if (result != B_OK) {
+		if (fReport.Device()->IsRemoved()) {
+			TRACE("device has been removed\n");
+			return B_DEV_NOT_READY;
+		}
+
+		if ((*cookie & PROTOCOL_HANDLER_COOKIE_FLAG_CLOSED) != 0)
+			return B_CANCELED;
+
+		if (result != B_INTERRUPTED) {
+			// interrupts happen when other reports come in on the same
+			// input as ours
+			TRACE_ALWAYS("error waiting for report: %s\n", strerror(result));
+		}
+
+		// signal that we simply want to try again
+		return B_INTERRUPTED;
+	}
+
+
+	return B_OK;
+}
---
+++ src/add-ons/kernel/drivers/input/hid_shared/RawProtocolHandler.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2022 Enrique Medina Gremaldos <quiqueiii@gmail.com>
+ * Distributed under the terms of the MIT license.
+ */
+
+#ifndef HID_RAW_PROTOCOL_HANDLER_H
+#define HID_RAW_PROTOCOL_HANDLER_H
+
+
+#include <InterfaceDefs.h>
+
+#include "ProtocolHandler.h"
+
+class HIDDevice;
+class HIDCollection;
+class HIDReportItem;
+
+
+class RawProtocolHandler : public ProtocolHandler {
+public:
+							RawProtocolHandler(HIDReport &report);
+
+	static	void			AddHandlers(HIDDevice &device,
+									HIDCollection &collection,
+									ProtocolHandler *&handlerList);
+
+	virtual	status_t		Control(uint32 *cookie, uint32 op, void *buffer,
+									size_t length);
+
+	virtual	status_t		Read(uint32 *cookie, off_t position,
+									void *buffer, size_t *numBytes);
+
+private:
+	status_t				_ReadReport(void *buffer, uint32 *cookie);
+			//HIDReport &		fReport;
+			//HIDDevice *		fDevice;
+};
+
+#endif
